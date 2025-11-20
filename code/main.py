a265fe1d197234702b0384a90e24f426fd001341
bedbug57 <a9910765@gmail.com>
2025-11-20 22:36:28 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 from neural_net import recommendations
 import numpy as np
-def get_jobs(inp, ): #inp=[0.12, 0.98, 0.44...]
+def get_jobs(inp): #inp=[0.12, 0.98, 0.44...]
     pers_input=np.array(inp)
     top=recommendations(pers_input, 1, 5)
     return top #top[0][i]=jobs, top[1][i]=scores for jobs(needs to be *100 to return precent)
