9dd14d378b8a284ac3e4f5abe3f28e88be2d7b20
Konrad <konradkrol110@gmail.com>
2025-11-24 17:23:00 +0100

    Replace TensorFlow with tflite-runtime

--- code/career_adviser.py
+++ code/career_adviser.py
@@ -1,9 +1,9 @@
-from code.neural_net import recommendations
+from code.neural_net_lite import recommendations_tflite
 import numpy as np
 
 
 def get_jobs(inp, wpep_mode, j_print):  # inp=[0.12, 0.98, 0.44...]
     pers_input = np.array(inp)
-    top = recommendations(pers_input, wpep_mode, j_print)
+    top = recommendations_tflite(pers_input, wpep_mode, j_print)
     # top[0][i]=jobs, top[1][i]=scores for jobs(needs to be *100 to return precent)
     return top
--- code/neural_net_lite.py
+++ code/neural_net_lite.py
@@ -1,54 +1,66 @@
 import numpy as np
-import tensorflow as tf
 
-job=["Strażak", "Policjant", "Ratownik medyczny", "Żołnierz", "Data scientist", "Data analyst", "Business analyst", 
-       "Specjalista ds cyberbezpieczeństwa", "Programista", "Statystyk", "Matematyk", "Fizyk", "Kierownik projektu", 
+try:
+    # Próbuj użyć tflite_runtime (lżejsza wersja, tylko do inferencji)
+    from tflite_runtime.interpreter import Interpreter
+except ImportError:
+    # Fallback do tensorflow.lite jeśli tflite_runtime nie jest dostępne
+    import tensorflow as tf
+    Interpreter = tf.lite.Interpreter
+
+job = ["Strażak", "Policjant", "Ratownik medyczny", "Żołnierz", "Data scientist", "Data analyst", "Business analyst",
+       "Specjalista ds cyberbezpieczeństwa", "Programista", "Statystyk", "Matematyk", "Fizyk", "Kierownik projektu",
        "Grafik komputerowy", "Projektant UI/UX", "Pracownik HR", "Konsultant IT", "Tester oprogramowania", "Trener personalny",
-       "Ogrodnik", "Lekarz", "Weterynarz", "Farmaceuta", "Stomatolog", "Pielęgniarka", "Fizjoterapeuta", "Psycholog", 
-       "Psychiatra", "Coach", "Hydraulik", "Stolarz", "Mechanik", "Elektryk", "Kierownik magazynu", "Operator maszyn", 
+       "Ogrodnik", "Lekarz", "Weterynarz", "Farmaceuta", "Stomatolog", "Pielęgniarka", "Fizjoterapeuta", "Psycholog",
+       "Psychiatra", "Coach", "Hydraulik", "Stolarz", "Mechanik", "Elektryk", "Kierownik magazynu", "Operator maszyn",
        "Projektant wnętrz", "Urbanista", "Kucharz", "Kelner", "Reżyser", "Aktor", "Muzyk", "Fotograf", "Pisarz", "Architekt",
-       "Montażysta wideo", "Realizator dźwięku", "Kierownik sklepu", "Przedstawiciel handlowy", "Specjalista ds marketingu", 
-       "Specjalista ds social mediów", "Tłumacz", "Doradca_podatkowy", "Prawnik", "Dziennikarz", "Analityk finansowy", 
-       "Księgowy", "Agent ubezpieczeniowy", "Pracownik biura podróży", "Pilot samolotu", "Maszynista", "Kierowca", "Logistyk", 
+       "Montażysta wideo", "Realizator dźwięku", "Kierownik sklepu", "Przedstawiciel handlowy", "Specjalista ds marketingu",
+       "Specjalista ds social mediów", "Tłumacz", "Doradca_podatkowy", "Prawnik", "Dziennikarz", "Analityk finansowy",
+       "Księgowy", "Agent ubezpieczeniowy", "Pracownik biura podróży", "Pilot samolotu", "Maszynista", "Kierowca", "Logistyk",
        "Nauczyciel", "Bibliotekarz", "Naukowiec", "Fryzjer", "Kosmetyczka", "Przedsiębiorca", "Komornik"]
 
+
 def normalise(x, e=0.01):
-    min_val=x.min()
-    max_val=x.max()
-    normali=(x-min_val)/(max_val-min_val)
-    normali=normali*(1-2*e)+e
+    min_val = x.min()
+    max_val = x.max()
+    normali = (x-min_val)/(max_val-min_val)
+    normali = normali*(1-2*e)+e
     return normali
+
 
 def transpose(matrix):
     return list(map(list, zip(*matrix)))
 
-def recommendations_tflite(personality_vector, wpep_mode, job_count):
-    interpreter=tf.lite.Interpreter(model_path="data/model.tflite")
-    interpreter.allocate_tensors()
-    input_details=interpreter.get_input_details()
-    output_details=interpreter.get_output_details()
 
-    input_data=np.array([personality_vector], dtype=input_details[0]['dtype'])
+def recommendations_tflite(personality_vector, wpep_mode, job_count):
+    interpreter = Interpreter(model_path="data/model.tflite")
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    input_data = np.array([personality_vector],
+                          dtype=input_details[0]['dtype'])
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
-    output_data=interpreter.get_tensor(output_details[0]['index'])[0]
-    top_indices=np.argsort(output_data)[-job_count:][::-1]
-    recom=[(job[i], output_data[i]) for i in top_indices]
-    trecom=transpose(recom)
-    outscores=np.array(trecom[1])
-    if wpep_mode==0:
-        nincome=np.ones(len(top_indices))
+    output_data = interpreter.get_tensor(output_details[0]['index'])[0]
+    top_indices = np.argsort(output_data)[-job_count:][::-1]
+    recom = [(job[i], output_data[i]) for i in top_indices]
+    trecom = transpose(recom)
+    outscores = np.array(trecom[1])
+    if wpep_mode == 0:
+        nincome = np.ones(len(top_indices))
     else:
-        if wpep_mode==1:
-            income=np.loadtxt("data/inout/wpep.csv", delimiter=',')
-        if wpep_mode==2:
-            income=np.loadtxt("data/inout/wpep5years.csv", delimiter=',')
-        tincome=[income[i] for i in top_indices]
-        nincome=normalise(np.array(tincome))
-    out=outscores-(1-nincome)
-    nout=normalise(out)
+        if wpep_mode == 1:
+            income = np.loadtxt("data/inout/wpep.csv", delimiter=',')
+        if wpep_mode == 2:
+            income = np.loadtxt("data/inout/wpep5years.csv", delimiter=',')
+        tincome = [income[i] for i in top_indices]
+        nincome = normalise(np.array(tincome))
+    out = outscores-(1-nincome)
+    nout = normalise(out)
     return trecom[0], nout
 
-personality=np.random.rand(25)
-jobs, scores=recommendations_tflite(personality, 1, 5)
+
+personality = np.random.rand(25)
+jobs, scores = recommendations_tflite(personality, 1, 5)
 print(list(zip(jobs, scores)))
